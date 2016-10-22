7540ceae6095b57fed30257d04add243151d99a3
Junichi Ito <jit@sonicgarden.jp>
2016-10-22 16:23:21 +0900

    Use transaction to save time

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 require 'benchmark'
 
 RSpec.describe Post, type: :model do
-  describe 'popular scope' do
-    let(:post_count) { 100 }
-    before do
+  let(:post_count) { 100 }
+  before do
+    Post.transaction do
       # Create posts with comments and likes
       post_count.times do |i|
         post = Post.create!(title: "Post #{i}")
@@ -17,38 +17,38 @@ RSpec.describe Post, type: :model do
       # Create a post without comment and like
       Post.create!(title: 'Hidden post')
     end
+  end
 
-    example 'benchmark' do
-      posts_with_uniq = nil
-      posts_with_exists = nil
+  example 'benchmark' do
+    posts_with_uniq = nil
+    posts_with_exists = nil
 
-      label_width = 7
-      results = Benchmark.bmbm(label_width) do |x|
-        x.report("Uniq:") do
-          posts_with_uniq = Post.joins(:comments, :likes).distinct
-          expect(posts_with_uniq.to_a.size).to eq post_count
-          expect(posts_with_uniq.map(&:title)).to_not include 'Hidden post'
-        end
+    label_width = 7
+    results = Benchmark.bmbm(label_width) do |x|
+      x.report("Uniq:") do
+        posts_with_uniq = Post.joins(:comments, :likes).distinct
+        expect(posts_with_uniq.to_a.size).to eq post_count
+        expect(posts_with_uniq.map(&:title)).to_not include 'Hidden post'
+      end
 
-        x.report("Exists:") do
-          posts_with_exists = Post.where <<-SQL.squish
+      x.report("Exists:") do
+        posts_with_exists = Post.where <<-SQL.squish
 EXISTS (SELECT * FROM comments c WHERE c.post_id = posts.id)
 AND
 EXISTS (SELECT * FROM likes l WHERE l.post_id = posts.id)
-          SQL
-          expect(posts_with_exists.to_a.size).to eq post_count
-          expect(posts_with_exists.map(&:title)).to_not include 'Hidden post'
-        end
+        SQL
+        expect(posts_with_exists.to_a.size).to eq post_count
+        expect(posts_with_exists.map(&:title)).to_not include 'Hidden post'
       end
-
-      diff = results[0].real / results[1].real
-      puts "\nExists is #{diff.round(6)} times faster."
-
-      puts "\nUniq:"
-      puts posts_with_uniq.explain
-
-      puts "\nExists"
-      puts posts_with_exists.explain
     end
+
+    diff = results[0].real / results[1].real
+    puts "\nExists is #{diff.round(6)} times faster."
+
+    puts "\nUniq:"
+    puts posts_with_uniq.explain
+
+    puts "\nExists"
+    puts posts_with_exists.explain
   end
 end
